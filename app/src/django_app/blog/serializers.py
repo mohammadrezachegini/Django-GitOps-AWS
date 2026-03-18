8ae2f1f39635e3d8a94cd46508e643c343e5c3d3
Reza <mohammadeza.chegini01@gmail.com>
2026-03-18 10:15:08 -0700

    transfer commit

--- app/src/django_app/blog/serializers.py
+++ app/src/django_app/blog/serializers.py
@@ -10,7 +10,7 @@ class UserSerializer(serializers.ModelSerializer):
     """
 
     class Meta:
-        model = User
+        model = Users
         fields = ["id", "username", "email", "date_joined"]
         read_only_fields = ["date_joined"]
 
